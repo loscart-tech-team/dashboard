9b3f724825486b10f5208625ac8abf9ea27240e9
Burak Cihan <burakcihan622@gmail.com>
2023-03-10 17:19:31 +0300

    düzenleme

--- src/components/myAccount/DeleteAccount/DeleteAccount.js
+++ src/components/myAccount/DeleteAccount/DeleteAccount.js
@@ -2,27 +2,27 @@ import React from "react";
 
 const DeleteAccount = () => {
   return (
-      <div className="p-6 border-gray-200 rounded-lg shadow mt-5">
-          <p className="font-semibold text-lg md:text-xl">Delete Account</p>
-          <div className="h-[80px] md:h-auto bg-orange-200 rounded-md p-2 mt-4 md:mt-6">
-              <h5 className="font-semibold text-lg md:text-xl text-amber-600">
-                  Are you sure you want to delete your account?
-              </h5>
-              <p className="text-md md:text-lg font-medium text-amber-600">
-                  Once you delete your account, there is no going back. Please be certain.
-              </p>
-          </div>
-          <div className="flex mt-4 md:mt-6">
-              <input type="checkbox" className="mr-2 text-md md:text-lg" />
-              <p className="text-md md:text-lg">I confirm my account deactivation</p>
-          </div>
-          <div className="flex mt-4 md:mt-6">
-              <button className="bg-red-500 text-white px-4 py-2 rounded-md text-md md:text-lg">
-                  Delete Account
-              </button>
-          </div>
+    <div className="p-6 border-gray-200 rounded-lg shadow mt-5">
+      <p className="font-semibold text-lg md:text-xl">Delete Account</p>
+      <div className="h-[80px] md:h-auto bg-orange-100 rounded-md p-4 mt-4 md:mt-6">
+        <h5 className="font-semibold text-lg md:text-xl text-amber-600">
+          Are you sure you want to delete your account?
+        </h5>
+        <p className="text-md md:text-lg font-medium text-amber-600">
+          Once you delete your account, there is no going back. Please be
+          certain.
+        </p>
       </div>
-
+      <div className="flex mt-4 md:mt-6">
+        <input type="checkbox" className="mr-2 text-md md:text-lg" />
+        <p className="text-md md:text-lg">I confirm my account deactivation</p>
+      </div>
+      <div className="flex mt-4 md:mt-6">
+        <button className="bg-red-500 hover:bg-red-800 text-white px-4 py-2 rounded-md text-md md:text-lg">
+          Deactivate Account
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/components/myAccount/ProfilDetails/ProfilDetails.js
+++ src/components/myAccount/ProfilDetails/ProfilDetails.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ProfilImages from "../ProfilImages/ProfilImages";
 
 const ProfilDetails = () => {
   return (
@@ -7,28 +8,8 @@ const ProfilDetails = () => {
         <div>
           <p className="font-semibold text-lg md:text-xl">Profil Details</p>
         </div>
-        <div className="flex flex-col items-center md:flex-row md:max-w-xl">
-          <img
-            className="rounded-lg h-24 w-24 md:h-40 md:w-40"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
-            alt=""
-          />
-          <div className="flex flex-col justify-between p-4 leading-normal w-full">
-            <div className="flex flex-col md:flex-row">
-              <button className="w-full md:w-40 flex flex-row gap-x-2 h-8 md:h-10 items-center justify-center bg-zinc-200 hover:bg-blue-600 hover:text-white active:bg-blue-700 rounded-md focus:bg-blue-700 focus:text-white">
-                Upload new photo
-              </button>
-              <button className="w-full md:w-40 flex flex-row gap-x-2 h-8 md:h-10 items-center justify-center bg-zinc-200 hover:bg-blue-600 hover:text-white active:bg-blue-700 rounded-md focus:bg-blue-700 focus:text-white mt-2 md:mt-0 md:ml-2">
-                Reset
-              </button>
-            </div>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 mt-3">
-              Allowed JPG, GIF or PNG. Max size of 800K
-            </p>
-          </div>
-        </div>
+        <ProfilImages />
       </div>
-
       <div className="w-full mt-3 border-gray-200 rounded-lg shadow p-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div className="col-span-1">
